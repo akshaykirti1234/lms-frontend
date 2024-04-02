4e82ba61f261b8eab3b561c026daf95c1caf479e
Chinmaya Jena <chinmaya.jena@corp.csmpl.com>
2024-04-02 18:06:49 +0530

    OTP based change password completed

--- src/app/auth/Components/forgot-password/forgot-password.component.ts
+++ src/app/auth/Components/forgot-password/forgot-password.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { LoginService } from '../../Services/login.service';
+import { map, Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,5 +10,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent {
+  resetForm!: FormGroup;
+  submitted = false;
+  loading = false;
 
+  constructor(private formBuilder: FormBuilder , private loginService : LoginService , private router : Router) { }
+
+  ngOnInit(): void {
+    this.resetForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  get f() { return this.resetForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.resetForm.invalid) {
+      return;
+    }
+    this.loading = true;
+    console.log(this.resetForm.value);
+    
+    setTimeout(() => {
+      this.loading = false;
+      this.loginService.checkEmail(this.resetForm.value.email).subscribe((data)=>{
+        if(data == true){
+          this.router.navigate(['/auth/otp/' , this.resetForm.value.email]);
+        }
+      },(error)=>{
+          console.log(error);
+      })
+    }, 2000);
+  }
+
+  
 }
+
+ 
