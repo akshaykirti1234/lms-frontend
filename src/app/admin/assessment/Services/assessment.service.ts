1d208929bd813d62e20aee2ec63cfd0248366831
Priyankapriyadarsinibiswal <priyanka.biswal@csm.tech>
2024-04-30 21:13:00 +0530

    some modification

--- src/app/admin/assessment/Services/assessment.service.ts
+++ src/app/admin/assessment/Services/assessment.service.ts
@@ -8,6 +8,8 @@ import { Observable } from 'rxjs';
 export class AssessmentService {
 
   private scheduleUrl="http://localhost:8085/";
+
+  private scheduleSesUrl="http://localhost:8085/api/sessionAssessment/";
   
 
   constructor(private httpClient:HttpClient) { }
@@ -36,6 +38,55 @@ export class AssessmentService {
     
     return this.httpClient.delete(this.scheduleUrl+`delete/${id}`);
   }
+
+  // for session
+
+  saveAssessmentSession(assessment:any):Observable<any>{
+    return this.httpClient.post(this.scheduleSesUrl+"assessmentSessionSave",assessment);
+  }
+
+  viewSessionAssessment():Observable<any>{
+    return this.httpClient.get(this.scheduleSesUrl+"viewAssessmentForSession");
+  }
+
+  deleteAssSession(id:any):Observable<any>{
+    console.log(id);
+    
+    return this.httpClient.delete(this.scheduleSesUrl+`deleteAssSession/${id}`);
+  }
+  editAssessmentSession(id:any):Observable<any>{
+    return this.httpClient.get(this.scheduleSesUrl+`editAssessmentSession/${id}`);
+  }
+
+// for session upload
+  
+  downloadSessionExcel() {
+
+    return this.httpClient.get("http://localhost:8085/downloadExcelSessionQuestions", { responseType: 'blob' })
+
+
+  }
+
+  importSessionExcel(formData: any) {
+
+    return this.httpClient.post("http://localhost:8085/uploadSessionExcel", formData, { responseType: 'text' })
+
+  }
+
+  // for schedule excel
+  downloadScheduleExcel() {
+
+    return this.httpClient.get("http://localhost:8085/downloadExcelmoduleQuestions", { responseType: 'blob' })
+
+
+  }
+
+  importScheduleExcel(formData: any) {
+
+    return this.httpClient.post("http://localhost:8085/uploadModuleExcel", formData, { responseType: 'text' })
+
+  }
+
 
 }
 
